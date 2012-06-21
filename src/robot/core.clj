09f225dc419481a0eda6416a586131cb8f2ffde9
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-06-21 15:45:33 +0100

    Moved store and retrieve to functions
    - Updated README

--- src/robot/core.clj
+++ src/robot/core.clj
@@ -6,13 +6,19 @@
 
 (def my-atom (atom {}))
 
+(defn store [id moves]
+  (reset! my-atom moves))
+
+(defn retrieve [id]
+  @my-atom)
+
 (defroutes main-routes
   (GET "/user/:id/moves" [id]
-    (str "User id: " id " has object " @my-atom))
+    (str (retrieve id)))
   (PUT "/user/:id/moves" [id :as req]
     (do
-      (reset! my-atom (parse-string (slurp (req :body))))
-      (str "Putted for user: " id)))
+      (store id (parse-string (slurp (req :body))))
+      (str "OK: " id)))
   (ben/resources "/")
   (ben/not-found "Page not found"))
 
