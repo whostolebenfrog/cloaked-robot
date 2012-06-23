3bff055c9c085b2389b5af65e7c7d1f1c170b859
Ben Griffiths <mrbengriffiths@googlemail.com>
2012-06-24 00:28:54 +0100

    Re-added url parsing, added get by move num
    Turns out url parsing isn't implemented (connects to a db with name :uri
    - DOH! - totally looked like it was working in the repl honest!)
    
    Updated the paths, added get last (by move) and get by move id

--- src/robot/core.clj
+++ src/robot/core.clj
@@ -6,36 +6,52 @@
   (:require [compojure.route :as ben]
             [compojure.handler :as handler]))
 
-;; custom encoder for the mongo id type
 (add-encoder org.bson.types.ObjectId encode-str)
+
+(defn split-mongo-url [url]
+  "split heroku mongo string into parts (mongodb://user:pass@localhost:1234/db)"
+  (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)]
+    (zipmap [:match :user :pass :host :port :db] (re-find matcher))))
 
 (defn get-mongo-url []
   (if (blank? (System/getProperty "MONGOHQ_URL"))
-    "mongodb://localhost:27017/robot"
+    "mongodb://:@localhost:27017/robot"
     (System/getProperty "MONGOHQ_URL")))
 
-;; inits mongo with either the MONGOHQ_URL system property or
-;; the default mongo port on localhost
 (defn init-mongo []
   "Initialise mongo using url from system property"
-  (do
-    (def conn
-      (make-connection :uri get-mongo-url))
-    (set-connection! conn)))
+  (let [params (split-mongo-url (get-mongo-url))]
+    (do
+      (prn params)
+      (def conn
+        (make-connection
+         (:db params)
+         :host (:host params)
+         :port (Integer. (:port params))))
+      (when (and
+             (not (blank? (:pass params)))
+             (not (blank? (:user params))))
+        (authenticate conn (:user params) (:pass (params))))
+      (set-connection! conn))))
 
 (defn store [moves]
   (insert! :moves moves))
 
-(defn retrieve [id]
-  (generate-string (fetch-one :moves)))
+(defn retrieve
+  ([id]
+     (generate-string (fetch :moves :where {:user id} :sort {:move -1} :limit 1)))
+  ([id move]
+     (generate-string (fetch-one :moves :where {:user id :move move}))))
 
 (defn with-user [id moves]
   (assoc moves :user id))
 
 (defroutes main-routes
-  (GET "/user/:id/moves" [id]
-    (str (retrieve id)))
-  (PUT "/user/:id/moves" [id :as req]
+  (GET "/users/:id/moves/last" [id]
+    (retrieve id))
+  (GET "/users/:id/moves/:move" [id move]
+    (retrieve id (Integer. move)))
+  (PUT "/users/:id/moves" [id :as req]
     (do
       (store (with-user id (parse-string (slurp (req :body)))))
       (str "OK: " id)))
