8194a2196b73980c2cba535e3c511f0ae4dd48fc
Ben Griffiths <mrbengriffiths@googlemail.com>
2012-06-23 23:31:00 +0100

    Write and read objects from mongo

--- src/robot/core.clj
+++ src/robot/core.clj
@@ -1,40 +1,57 @@
 (ns robot.core
   (:use [compojure.core]
-        [cheshire.core]
-        [somnium.congomongo])
+        [cheshire.custom]
+        [somnium.congomongo]
+        [clojure.string :only (blank?)])
   (:require [compojure.route :as ben]
             [compojure.handler :as handler]))
+
+;; custom encoder for the mongo id type
+(add-encoder org.bson.types.ObjectId encode-str)
 
 (defn split-mongo-url [url]
   "split heroku mongo string into parts (mongodb://user:pass@localhost:1234/db)"
   (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)]
     (zipmap [:match :user :pass :host :port :db] (re-find matcher))))
 
+(defn get-mongo-url []
+  (if (blank? (System/getProperty "MONGOHQ_URL"))
+    "mongodb://user:pass@localhost:27017/robot"
+    (System/getProperty "MONGOHQ_URL")))
+
+;; inits mongo with either the MONGOHQ_URL system property or
+;; the default mongo port on localhost
 (defn init-mongo []
   "Initialise mongo using url from system property"
-  (let [params (split-mongo-url (get (System/getenv) "MONGOHQ_URL"))]
-    (make-connection
-     (:db params)
-     :host (:host params)
-     :port (:port params)
-     :user (:user params)
-     :pass (:pass params))))
+  (let [params (split-mongo-url (get-mongo-url))]
+    (do
+      (def conn
+        (make-connection
+         (:db params)
+         :host (:host params)
+         :port (Integer. (:port params))))
+      (set-connection! conn))))
 
-(defn store [id moves]
-  ())
+(defn store [moves]
+  (insert! :moves moves))
 
 (defn retrieve [id]
-  {})
+  (generate-string (fetch-one :moves)))
+
+(defn with-user [id moves]
+  (assoc moves :user id))
 
 (defroutes main-routes
   (GET "/user/:id/moves" [id]
     (str (retrieve id)))
   (PUT "/user/:id/moves" [id :as req]
     (do
-      (store id (parse-string (slurp (req :body))))
+      (store (with-user id (parse-string (slurp (req :body)))))
       (str "OK: " id)))
   (ben/resources "/")
   (ben/not-found "Page not found"))
+
+(init-mongo)
 
 (def app
   (handler/site main-routes))
